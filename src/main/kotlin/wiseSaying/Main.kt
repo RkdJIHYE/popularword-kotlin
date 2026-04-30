3409b081a8a1f67badda669f624c7dabf489be94
RkdJIHYE <kangbs210210@gmail.com>
2026-04-30 11:22:24 +0900

    Feat : 코드 리팩토링

--- src/main/kotlin/wiseSaying/Main.kt
+++ src/main/kotlin/wiseSaying/Main.kt
@@ -1,25 +1,7 @@
 package wiseSaying
 
-fun main(){
-    println("== 명언 앱 ==")
-    var lastId = 0;
-    while (true){
-        print("명령) ")
+import kotlin.run
 
-        val input = readln()
-        when (input){
-            "종료" ->{
-                println("프로그램을 종료합니다.")
-                break
-            }
-            "등록" ->{
-                print("명언 : ")
-                val  content = readln()
-                print("작가 : ")
-                val author = readln()
-                val id = ++lastId
-                println("${id}번 명령이 등록되었습니다.")
-            }
-        }
-    }
+fun main(){
+    App().run()
 }
